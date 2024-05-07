d82f074cb8cc76f9d085c13f081e19b4c02e0623
thanujsai <sboda@modeln.com>
2024-05-07 10:07:46 +0530

    search bar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,29 @@
 // import {Menu} from "antd"; 
-import { Sidebar, Menu,MenuItem, SubMenu } from 'antd';
+import { Sidebar, Menu,MenuItem, SubMenu , Input} from 'antd';
+import { useNavigate } from 'react-router-dom';
 
 function Navbar(){
+
+    var searchText;
+    var navigate = useNavigate();
+    function search(){
+        console.log('user is searching for ',searchText)
+        var url = "/search?q="+searchText;
+        navigate(url)
+        //navigate to above url
+    }
+
+    function handleSearchText(e){
+        searchText = e.target.value;
+        console.log()
+    }
     const items = [
         {label:'Home'},
         {label:'About'},
-        {label:'Information'}
+        {label:'Information'},
+        {label:
+            (<Input placeholder="Search" onChange={handleSearchText}></Input>)
+        },
     ]
     return(
         <>
--- src/Components/Signupp.js
+++ src/Components/Signupp.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import {Input,Button,Alert} from 'antd'
 import { useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 function Signupp() {
   var [name,setName] = useState("");
   var [email,setEmail] = useState("");
   var [password,setPassword] = useState("");
 
+  function CreateAccount(){
+    console.log(name,' ',email,' ',password)
+    axios({
+        url:"http://apibyauw.eu-4.evennode.com/api"+"/register",
+        method:"post",
+        data:{name,email,password}
+    }).then((response) => {
+        console.log(response)
+        console.log("data is")
+        console.log(response.data)
+        console.log("response coming from create account api ",response.data.data)
+    }, (error) => {
+        console.log("error from register/create account api",error)
+    })
+}
   console.log("naam hai : ",name)
   function yo(){
     alert("name is : "+name+"\nemail is : "+email+"\npassword is : "+password)
@@ -34,6 +51,8 @@ function Signupp() {
         <input placeholder="Password" type="password" onChange={passwordChange}></input><br></br>
         <input placeholder="Confirm Password" type="password" onChange={passwordChange}></input><br></br><br></br>
         <Button onClick = {yo}>Sign Up</Button>
+        <Button onClick = {CreateAccount}>Create Account</Button>
+        <Link to='/login'><Button>Sign In</Button></Link>
     </div>
   )
 }
